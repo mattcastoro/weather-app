f5ef82512aec9c64067c0f990393d7a807a3df86
Matt Castoro <matthewjcastoro@gmail.com>
2025-02-24 10:46:53 -0700

    Call API and return weather data
    Setup API call to Visual Crossings.  Include basic error handling.
    Parse returned data to be populated within the user's display.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hi there");
+const apiKey = 'PN8RHXTN8SN6DFG8DGBSBE9GS'
+
+async function getData(location, unit) {
+  try {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unit}&key=${apiKey}`);
+    if (!response.ok) {
+      throw 'something went wrong!';
+    }
+    const data = await response.json();
+    return data;
+  } catch(error) {
+    console.warn(error);
+  }
+  
+}
+
+getData("boulder", "us").then((data) => {
+  console.log(data);
+  const displayData = {
+    datetime : data.currentConditions.datetime,
+    location : data.resolvedAddress,
+    currentTemp : data.currentConditions.temp,
+    feelsLike : data.currentConditions.feelslike,
+    currentHighTemp : data.days[0].tempmax,
+    currentLowTemp : data.days[0].tempmin,
+    currentDayDescription : data.days[0].description,
+  };
+  console.log(displayData);
+})
