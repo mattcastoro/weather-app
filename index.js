8682004749bac66acd6910529d59ba2d195ce099
Matt Castoro <matthewjcastoro@gmail.com>
2025-02-25 15:39:36 -0700

    Test for displaying pulled data
    Write simple function to use pulled data to display an image related
    to the weather icon data point.

--- index.js
+++ index.js
@@ -1,63 +1,64 @@
 const apiKey = 'PN8RHXTN8SN6DFG8DGBSBE9GS'
 
 async function getData(location, unit) {
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unit}&key=${apiKey}`
   try {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unit}&key=${apiKey}`);
+    const response = await fetch(url);
     if (!response.ok) {
-      throw 'something went wrong!';
+      throw new Error(`Response status: ${response.status}`);
     }
     const data = await response.json();
     return data;
   } catch(error) {
-    console.warn(error);
+    console.warn(error.message);
   }
-  
 }
 
-let displayData = {};
-getData("boulder", "us").then((data) => {
-  console.log(data);
-  const hour = data.currentConditions.datetime.slice(0,2);
-  displayData = {
-    datetime : data.currentConditions.datetime,
-    location : data.resolvedAddress,
+async function getWeather() {
+  console.log("I've been clicked!");
+  getData("boulder", "us").then((data) => {
+    const hour = data.currentConditions.datetime.slice(0,2);
+    const displayData = {
+      datetime : data.currentConditions.datetime,
+      location : cleanLocation(data.resolvedAddress),
 
-    currentTemp : data.currentConditions.temp,
-    feelsLike : data.currentConditions.feelslike,
-    currentHighTemp : data.days[0].tempmax,
-    currentLowTemp : data.days[0].tempmin,
+      currentTemp : data.currentConditions.temp,
+      feelsLike : data.currentConditions.feelslike,
+      currentHighTemp : data.days[0].tempmax,
+      currentLowTemp : data.days[0].tempmin,
 
-    currentDayDescription : data.days[0].description,
-    hour0Icon : data.currentConditions.icon,
-    hour1Icon : data.days[0].hours[setHours(hour, 1)].icon,
-    hour1Temp : data.days[0].hours[setHours(hour, 1)].temp,
-    hour2Icon : data.days[0].hours[setHours(hour, 2)].icon,
-    hour2Temp : data.days[0].hours[setHours(hour, 2)].temp,
-    hour3Icon : data.days[0].hours[setHours(hour, 3)].icon,
-    hour3Temp : data.days[0].hours[setHours(hour, 3)].temp,
-    hour4Icon : data.days[0].hours[setHours(hour, 4)].icon,
-    hour4Temp : data.days[0].hours[setHours(hour, 4)].temp,
-    hour5Icon : data.days[0].hours[setHours(hour, 5)].icon,
-    hour5Temp : data.days[0].hours[setHours(hour, 5)].temp,
+      currentDayDescription : data.days[0].description,
+      hour0Icon : data.currentConditions.icon,
+      hour1Icon : data.days[0].hours[setHours(hour, 1)].icon,
+      hour1Temp : data.days[0].hours[setHours(hour, 1)].temp,
+      hour2Icon : data.days[0].hours[setHours(hour, 2)].icon,
+      hour2Temp : data.days[0].hours[setHours(hour, 2)].temp,
+      hour3Icon : data.days[0].hours[setHours(hour, 3)].icon,
+      hour3Temp : data.days[0].hours[setHours(hour, 3)].temp,
+      hour4Icon : data.days[0].hours[setHours(hour, 4)].icon,
+      hour4Temp : data.days[0].hours[setHours(hour, 4)].temp,
+      hour5Icon : data.days[0].hours[setHours(hour, 5)].icon,
+      hour5Temp : data.days[0].hours[setHours(hour, 5)].temp,
 
-    day1Icon : data.days[1].icon,
-    day1TempLow : data.days[1].tempmin,
-    day1TempHigh : data.days[1].tempmax,
-    day2Icon : data.days[2].icon,
-    day2TempLow : data.days[2].tempmin,
-    day2TempHigh : data.days[2].tempmax,
-    day3Icon : data.days[3].icon,
-    day3TempLow : data.days[3].tempmin,
-    day3TempHigh : data.days[3].tempmax,
-    day4Icon : data.days[4].icon,
-    day4TempLow : data.days[4].tempmin,
-    day4TempHigh : data.days[4].tempmax,
-    day5Icon : data.days[5].icon,
-    day5TempLow : data.days[5].tempmin,
-    day5TempHigh : data.days[5].tempmax,
-  };
-  console.log(displayData);
-}) 
+      day1Icon : data.days[1].icon,
+      day1TempLow : data.days[1].tempmin,
+      day1TempHigh : data.days[1].tempmax,
+      day2Icon : data.days[2].icon,
+      day2TempLow : data.days[2].tempmin,
+      day2TempHigh : data.days[2].tempmax,
+      day3Icon : data.days[3].icon,
+      day3TempLow : data.days[3].tempmin,
+      day3TempHigh : data.days[3].tempmax,
+      day4Icon : data.days[4].icon,
+      day4TempLow : data.days[4].tempmin,
+      day4TempHigh : data.days[4].tempmax,
+      day5Icon : data.days[5].icon,
+      day5TempLow : data.days[5].tempmin,
+      day5TempHigh : data.days[5].tempmax,
+    };
+    displayWeather(displayData);
+  })
+}
 
 function setHours(currentHour, shift) {
   if (Number(currentHour) + shift <= 24) {
@@ -65,6 +66,11 @@ function setHours(currentHour, shift) {
   } else {
     return (Number(currentHour) + shift) - 24;
   }
+}
+
+function cleanLocation(location) {
+  let split = location.split(",");
+  return `${split[0]},${split[1]}`;
 }
 
 function getIcon(iconId) {
@@ -91,8 +97,13 @@ function getIcon(iconId) {
       return "images/default.svg";
   }
 }
-const test = document.querySelector('.test')
 
-const image = document.createElement('img');
-image.src = getIcon("cloudy");
-test.appendChild(image);
+function displayWeather(displayData) {
+  const test = document.querySelector('.test')
+  const image = document.createElement('img');
+  image.src = getIcon(displayData.hour0Icon);
+  test.appendChild(image);
+  console.log(displayData.hour0Icon);
+}
+
+
