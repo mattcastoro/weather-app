6d9bee56076f6909155e251d3b7dfb5f90f8ed91
Matt Castoro <matthewjcastoro@gmail.com>
2025-02-26 12:33:01 -0700

    Display: current conditions and forecasts
    Add displays for current conditions, 5 hour forecast, and 5 day
    forecast.

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function getWeather() {
       feelsLike : data.currentConditions.feelslike,
       currentHighTemp : data.days[0].tempmax,
       currentLowTemp : data.days[0].tempmin,
+      currentIcon : data.days[0].icon,
 
       currentDayDescription : data.days[0].description,
       hour0Icon : data.currentConditions.icon,
@@ -63,12 +64,299 @@ async function getWeather() {
 }
 
 function displayWeather(displayData) {
+  const top = document.querySelector('.top');
 
+  const searchLocation = document.createElement('div');
+  top.appendChild(searchLocation);
+  searchLocation.textContent = displayData.location;
+  searchLocation.classList.add('heading');
 
-  const test = document.querySelector('.test')
-  const image = document.createElement('img');
-  image.src = getIcon(displayData.hour0Icon);
-  test.appendChild(image);
+  const currentTemp = document.createElement('div');
+  top.appendChild(currentTemp);
+  currentTemp.textContent = displayData.currentTemp;
+  currentTemp.classList.add('title');
+
+  const feelsLike = document.createElement('div');
+  top.appendChild(feelsLike);
+  feelsLike.textContent = `Feels Like: ${displayData.feelsLike}`;
+  feelsLike.classList.add('content');
+
+  const hontainerTemp = document.createElement('div');
+  top.appendChild(hontainerTemp);
+  hontainerTemp.classList.add('hontainer');
+
+  const currentHigh = document.createElement('div');
+  hontainerTemp.appendChild(currentHigh);
+  currentHigh.textContent = `H: ${displayData.currentHighTemp}`;
+  currentHigh.classList.add('content');
+  
+  const currentLow = document.createElement('div');
+  hontainerTemp.appendChild(currentLow);
+  currentLow.textContent = `L: ${displayData.currentLowTemp}`;
+  currentLow.classList.add('content');
+
+  const middle = document.querySelector('.middle');
+
+  const description = document.createElement('div');
+  middle.appendChild(description);
+  description.textContent = displayData.currentDayDescription;
+  description.classList.add('heading');
+
+  const hontainerHourly = document.createElement('div');
+  middle.appendChild(hontainerHourly);
+  hontainerHourly.classList.add('hontainer');
+
+  const vontainerHour0 = document.createElement('div');
+  hontainerHourly.appendChild(vontainerHour0);
+  vontainerHour0.classList.add('vontainer');
+
+  const time0 = document.createElement('div');
+  vontainerHour0.appendChild(time0);
+  time0.textContent = "Now";
+  time0.classList.add('content');
+
+  const iconTime0 = document.createElement('img');
+  vontainerHour0.appendChild(iconTime0);
+  iconTime0.src = getIcon(displayData.hour0Icon);
+  iconTime0.classList.add('content', 'icon');
+
+  const tempTime0 = document.createElement('div');
+  vontainerHour0.appendChild(tempTime0);
+  tempTime0.textContent = displayData.currentTemp;
+  tempTime0.classList.add('content');
+
+  const vontainerHour1 = document.createElement('div');
+  hontainerHourly.appendChild(vontainerHour1);
+  vontainerHour1.classList.add('vontainer');
+
+  const time1 = document.createElement('div');
+  vontainerHour1.appendChild(time1);
+  time1.textContent = setHours(displayData.datetime.slice(0, 2), 1);
+  time1.classList.add('content');
+
+  const iconTime1 = document.createElement('img');
+  vontainerHour1.appendChild(iconTime1);
+  iconTime1.src = getIcon(displayData.hour1Icon);
+  iconTime1.classList.add('content', 'icon');
+
+  const tempTime1 = document.createElement('div');
+  vontainerHour1.appendChild(tempTime1);
+  tempTime1.textContent = displayData.hour1Temp;
+  tempTime1.classList.add('content');
+
+  const vontainerHour2 = document.createElement('div');
+  hontainerHourly.appendChild(vontainerHour2);
+  vontainerHour2.classList.add('vontainer');
+
+  const time2 = document.createElement('div');
+  vontainerHour2.appendChild(time2);
+  time2.textContent = setHours(displayData.datetime.slice(0, 2), 2);
+  time2.classList.add('content');
+
+  const iconTime2 = document.createElement('img');
+  vontainerHour2.appendChild(iconTime2);
+  iconTime2.src = getIcon(displayData.hour2Icon);
+  iconTime2.classList.add('content', 'icon');
+
+  const tempTime2 = document.createElement('div');
+  vontainerHour2.appendChild(tempTime2);
+  tempTime2.textContent = displayData.hour2Temp;
+  tempTime2.classList.add('content');
+
+  const vontainerHour3 = document.createElement('div');
+  hontainerHourly.appendChild(vontainerHour3);
+  vontainerHour3.classList.add('vontainer');
+
+  const time3 = document.createElement('div');
+  vontainerHour3.appendChild(time3);
+  time3.textContent = setHours(displayData.datetime.slice(0, 2), 3);
+  time3.classList.add('content');
+
+  const iconTime3 = document.createElement('img');
+  vontainerHour3.appendChild(iconTime3);
+  iconTime3.src = getIcon(displayData.hour3Icon);
+  iconTime3.classList.add('content', 'icon');
+
+  const tempTime3 = document.createElement('div');
+  vontainerHour3.appendChild(tempTime3);
+  tempTime3.textContent = displayData.hour3Temp;
+  tempTime3.classList.add('content');
+
+  const vontainerHour4 = document.createElement('div');
+  hontainerHourly.appendChild(vontainerHour4);
+  vontainerHour4.classList.add('vontainer');
+
+  const time4 = document.createElement('div');
+  vontainerHour4.appendChild(time4);
+  time4.textContent = setHours(displayData.datetime.slice(0, 2), 4);
+  time4.classList.add('content');
+
+  const iconTime4 = document.createElement('img');
+  vontainerHour4.appendChild(iconTime4);
+  iconTime4.src = getIcon(displayData.hour4Icon);
+  iconTime4.classList.add('content', 'icon');
+
+  const tempTime4 = document.createElement('div');
+  vontainerHour4.appendChild(tempTime4);
+  tempTime4.textContent = displayData.hour4Temp;
+  tempTime4.classList.add('content');
+
+  const vontainerHour5 = document.createElement('div');
+  hontainerHourly.appendChild(vontainerHour5);
+  vontainerHour5.classList.add('vontainer');
+
+  const time5 = document.createElement('div');
+  vontainerHour5.appendChild(time5);
+  time5.textContent = setHours(displayData.datetime.slice(0, 2), 5);
+  time5.classList.add('content');
+
+  const iconTime5 = document.createElement('img');
+  vontainerHour5.appendChild(iconTime5);
+  iconTime5.src = getIcon(displayData.hour5Icon);
+  iconTime5.classList.add('content', 'icon');
+
+  const tempTime5 = document.createElement('div');
+  vontainerHour5.appendChild(tempTime5);
+  tempTime5.textContent = displayData.hour5Temp;
+  tempTime5.classList.add('content');
+
+  const bottom = document.querySelector('.bottom');
+
+  const dailyForecast = document.createElement('div');
+  bottom.appendChild(dailyForecast);
+  dailyForecast.textContent = "5-DAY FORECAST";
+  dailyForecast.classList.add('heading');
+
+  const hontainerDaily = document.createElement('div');
+  bottom.appendChild(hontainerDaily);
+  hontainerDaily.classList.add('hontainer');
+
+  const vontainerDays = document.createElement('div');
+  hontainerDaily.appendChild(vontainerDays);
+  vontainerDays.classList.add("vontainer");
+
+  const dayDay0 = document.createElement('div');
+  vontainerDays.appendChild(dayDay0);
+  dayDay0.textContent = "Today";
+  dayDay0.classList.add('content');
+
+  const dayDay1 = document.createElement('div');
+  vontainerDays.appendChild(dayDay1);
+  dayDay1.textContent = "Tomorrow";
+  dayDay1.classList.add('content');
+
+  const dayDay2 = document.createElement('div');
+  vontainerDays.appendChild(dayDay2);
+  dayDay2.textContent = ;
+  dayDay2.classList.add('content');
+
+  //
+
+  //
+
+  //
+
+  const vontainerIcon = document.createElement('div');
+  hontainerDaily.appendChild(vontainerIcon);
+  vontainerIcon.classList.add("vontainer");
+
+  const iconDay0 = document.createElement('img');
+  vontainerIcon.appendChild(iconDay0);
+  iconDay0.src = getIcon(displayData.currentIcon);
+  iconDay0.classList.add('content', 'icon');
+
+  const iconDay1 = document.createElement('img');
+  vontainerIcon.appendChild(iconDay1);
+  iconDay1.src = getIcon(displayData.day1Icon);
+  iconDay1.classList.add('content', 'icon');
+
+  const iconDay2 = document.createElement('img');
+  vontainerIcon.appendChild(iconDay2);
+  iconDay2.src = getIcon(displayData.day2Icon);
+  iconDay2.classList.add('content', 'icon');
+
+  const iconDay3 = document.createElement('img');
+  vontainerIcon.appendChild(iconDay3);
+  iconDay3.src = getIcon(displayData.day3Icon);
+  iconDay3.classList.add('content', 'icon');
+
+  const iconDay4 = document.createElement('img');
+  vontainerIcon.appendChild(iconDay4);
+  iconDay4.src = getIcon(displayData.day4Icon);
+  iconDay4.classList.add('content', 'icon');
+
+  const iconDay5 = document.createElement('img');
+  vontainerIcon.appendChild(iconDay5);
+  iconDay5.src = getIcon(displayData.day5Icon);
+  iconDay5.classList.add('content', 'icon');
+
+  const vontainerTempLow = document.createElement('div');
+  hontainerDaily.appendChild(vontainerTempLow);
+  vontainerTempLow.classList.add("vontainer");
+
+  const tempLowDay0 = document.createElement('div');
+  vontainerTempLow.appendChild(tempLowDay0);
+  tempLowDay0.textContent = displayData.currentLowTemp;
+  tempLowDay0.classList.add('content');
+
+  const tempLowDay1 = document.createElement('div');
+  vontainerTempLow.appendChild(tempLowDay1);
+  tempLowDay1.textContent = displayData.day1TempLow;
+  tempLowDay1.classList.add('content');
+
+  const tempLowDay2 = document.createElement('div');
+  vontainerTempLow.appendChild(tempLowDay2);
+  tempLowDay2.textContent = displayData.day2TempLow;
+  tempLowDay2.classList.add('content');
+
+  const tempLowDay3 = document.createElement('div');
+  vontainerTempLow.appendChild(tempLowDay3);
+  tempLowDay3.textContent = displayData.day3TempLow;
+  tempLowDay3.classList.add('content');
+
+  const tempLowDay4 = document.createElement('div');
+  vontainerTempLow.appendChild(tempLowDay4);
+  tempLowDay4.textContent = displayData.day4TempLow;
+  tempLowDay4.classList.add('content');
+
+  const tempLowDay5 = document.createElement('div');
+  vontainerTempLow.appendChild(tempLowDay5);
+  tempLowDay5.textContent = displayData.day5TempLow;
+  tempLowDay5.classList.add('content');
+
+  const vontainerTempHigh = document.createElement('div');
+  hontainerDaily.appendChild(vontainerTempHigh);
+  vontainerTempHigh.classList.add("vontainer");
+
+  const tempHighDay0 = document.createElement('div');
+  vontainerTempHigh.appendChild(tempHighDay0);
+  tempHighDay0.textContent = displayData.currentHighTemp;
+  tempHighDay0.classList.add('content');
+
+  const tempHighDay1 = document.createElement('div');
+  vontainerTempHigh.appendChild(tempHighDay1);
+  tempHighDay1.textContent = displayData.day1TempHigh;
+  tempHighDay1.classList.add('content');
+
+  const tempHighDay2 = document.createElement('div');
+  vontainerTempHigh.appendChild(tempHighDay2);
+  tempHighDay2.textContent = displayData.day2TempHigh;
+  tempHighDay2.classList.add('content');
+
+  const tempHighDay3 = document.createElement('div');
+  vontainerTempHigh.appendChild(tempHighDay3);
+  tempHighDay3.textContent = displayData.day3TempHigh;
+  tempHighDay3.classList.add('content');
+
+  const tempHighDay4 = document.createElement('div');
+  vontainerTempHigh.appendChild(tempHighDay4);
+  tempHighDay4.textContent = displayData.day4TempHigh;
+  tempHighDay4.classList.add('content');
+
+  const tempHighDay5 = document.createElement('div');
+  vontainerTempHigh.appendChild(tempHighDay5);
+  tempHighDay5.textContent = displayData.day5TempHigh;
+  tempHighDay5.classList.add('content');
 }
 
 function cleanLocation(location) {
@@ -82,6 +370,10 @@ function setHours(currentHour, shift) {
   } else {
     return (Number(currentHour) + shift) - 24;
   }
+}
+
+function setDay (currentDay, shift) {
+  
 }
 
 function getIcon(iconId) {
