7c596e60e038e08ccc3860d89b86cbfa3f894dbf
Matt Castoro <matthewjcastoro@gmail.com>
2026-02-17 10:57:28 -0700

    Combine API calls into single Object

--- index.js
+++ index.js
@@ -32,67 +32,141 @@ async function getDataPast(location, unit) {
 async function getWeather() {
   const userLocation = document.getElementById('userLocation');
   const unit = getUnit();
-  if (!userLocation.value) {
+
+  if(!userLocation.value) {
     alert("Please add a location before searching or selecting a degree unit.");
-  } else {
-    getDataPast(userLocation.value, unit).then((data) => {
-      console.log(data);
-    });
+    return;
+  }
 
-    getDataCurrent(userLocation.value, unit).then((data) => {
-      console.log(data);
-      const hour = data.currentConditions.datetime.slice(0,2);
-      const displayData = {
-        datetime : data.currentConditions.datetime,
-        location : cleanLocation(data.resolvedAddress),
+  try {
+    const [pastData, currentData] = await Promise.all([
+      getDataPast(userLocation.value, unit),
+      getDataCurrent(userLocation.value, unit)
+    ]);
 
-        currentTemp : Math.round(data.currentConditions.temp),
-        feelsLike : Math.round(data.currentConditions.feelslike),
-        currentHighTemp : Math.round(data.days[0].tempmax),
-        currentLowTemp : Math.round(data.days[0].tempmin),
-        currentIcon : data.currentConditions.icon,
-        currentSunrise : data.currentConditions.sunrise.slice(0, 5),
-        currentSunset : data.currentConditions.sunset.slice(0, 5),
-
-        currentDayDescription : data.description,
-        hour0Icon : data.currentConditions.icon,
-        hour1Icon : data.days[0].hours[setHours(hour, 1)].icon,
-        hour1Temp : Math.round(data.days[0].hours[setHours(hour, 1)].temp),
-        hour2Icon : data.days[0].hours[setHours(hour, 2)].icon,
-        hour2Temp : Math.round(data.days[0].hours[setHours(hour, 2)].temp),
-        hour3Icon : data.days[0].hours[setHours(hour, 3)].icon,
-        hour3Temp : Math.round(data.days[0].hours[setHours(hour, 3)].temp),
-        hour4Icon : data.days[0].hours[setHours(hour, 4)].icon,
-        hour4Temp : Math.round(data.days[0].hours[setHours(hour, 4)].temp),
-        hour5Icon : data.days[0].hours[setHours(hour, 5)].icon,
-        hour5Temp : Math.round(data.days[0].hours[setHours(hour, 5)].temp),
-
-        day1Day : data.days[1].datetime,
-        day1Icon : data.days[1].icon,
-        day1TempLow : Math.round(data.days[1].tempmin),
-        day1TempHigh : Math.round(data.days[1].tempmax),
-        day2Day : data.days[2].datetime,
-        day2Icon : data.days[2].icon,
-        day2TempLow : Math.round(data.days[2].tempmin),
-        day2TempHigh : Math.round(data.days[2].tempmax),
-        day3Day : data.days[3].datetime,
-        day3Icon : data.days[3].icon,
-        day3TempLow : Math.round(data.days[3].tempmin),
-        day3TempHigh : Math.round(data.days[3].tempmax),
-        day4Day : data.days[4].datetime,
-        day4Icon : data.days[4].icon,
-        day4TempLow : Math.round(data.days[4].tempmin),
-        day4TempHigh : Math.round(data.days[4].tempmax),
-        day5Day : data.days[5].datetime,
-        day5Icon : data.days[5].icon,
-        day5TempLow : Math.round(data.days[5].tempmin),
-        day5TempHigh : Math.round(data.days[5].tempmax),
-      };
-      console.log(displayData);
-      displayWeather(displayData);
-    })
+    const combinedData = {
+      past: pastData,
+      current: currentData
+    };
+    console.log(combinedData);
+  } catch (error) {
+    console.warn("Error fetching weather data:", error);
   }
 }
+
+// async function getWeather() {
+//   const userLocation = document.getElementById('userLocation');
+//   const unit = getUnit();
+//   if (!userLocation.value) {
+//     alert("Please add a location before searching or selecting a degree unit.");
+//   } else {
+//     getDataPast(userLocation.value, unit).then((data) => {
+//       console.log(data);
+//       const hour = new Date().getHours();
+//       console.log(hour);
+//       const displayData_ly = {
+//         datetime : new Date().toLocaleString(),
+//       }
+//       console.log(displayData_ly);
+//       const now = new Date().toLocaleDateString();
+//       const today = new Date();
+//       console.log("today's date " + "= " + now);
+//       console.log(today.getHours());
+//     });
+
+//     getDataCurrent(userLocation.value, unit).then((data) => {
+//       console.log(data);
+//       const hour = data.currentConditions.datetime.slice(0,2);
+//       console.log("today's hour " + "= " + hour);
+//       const displayData = {
+//         datetime : data.currentConditions.datetime,
+//         location : cleanLocation(data.resolvedAddress),
+
+//         currentTemp : Math.round(data.currentConditions.temp),
+//         feelsLike : Math.round(data.currentConditions.feelslike),
+//         currentHighTemp : Math.round(data.days[0].tempmax),
+//         currentLowTemp : Math.round(data.days[0].tempmin),
+//         currentIcon : data.currentConditions.icon,
+//         currentSunrise : data.currentConditions.sunrise.slice(0, 5),
+//         currentSunset : data.currentConditions.sunset.slice(0, 5),
+
+//         currentDayDescription : data.description,
+//         hour0Icon : data.currentConditions.icon,
+//         hour1Icon : data.days[0].hours[setHours(hour, 1)].icon,
+//         hour1Temp : Math.round(data.days[0].hours[setHours(hour, 1)].temp),
+//         hour2Icon : data.days[0].hours[setHours(hour, 2)].icon,
+//         hour2Temp : Math.round(data.days[0].hours[setHours(hour, 2)].temp),
+//         hour3Icon : data.days[0].hours[setHours(hour, 3)].icon,
+//         hour3Temp : Math.round(data.days[0].hours[setHours(hour, 3)].temp),
+//         hour4Icon : data.days[0].hours[setHours(hour, 4)].icon,
+//         hour4Temp : Math.round(data.days[0].hours[setHours(hour, 4)].temp),
+//         hour5Icon : data.days[0].hours[setHours(hour, 5)].icon,
+//         hour5Temp : Math.round(data.days[0].hours[setHours(hour, 5)].temp),
+
+//         day1Day : data.days[1].datetime,
+//         day1Icon : data.days[1].icon,
+//         day1TempLow : Math.round(data.days[1].tempmin),
+//         day1TempHigh : Math.round(data.days[1].tempmax),
+//         day2Day : data.days[2].datetime,
+//         day2Icon : data.days[2].icon,
+//         day2TempLow : Math.round(data.days[2].tempmin),
+//         day2TempHigh : Math.round(data.days[2].tempmax),
+//         day3Day : data.days[3].datetime,
+//         day3Icon : data.days[3].icon,
+//         day3TempLow : Math.round(data.days[3].tempmin),
+//         day3TempHigh : Math.round(data.days[3].tempmax),
+//         day4Day : data.days[4].datetime,
+//         day4Icon : data.days[4].icon,
+//         day4TempLow : Math.round(data.days[4].tempmin),
+//         day4TempHigh : Math.round(data.days[4].tempmax),
+//         day5Day : data.days[5].datetime,
+//         day5Icon : data.days[5].icon,
+//         day5TempLow : Math.round(data.days[5].tempmin),
+//         day5TempHigh : Math.round(data.days[5].tempmax),
+
+//         ly_hour0Icon : data.currentConditions.icon,
+//         ly_hour0Temp : Math.round(data.currentConditions.temp),
+//         ly_hour1Icon : data.days[0].hours[setHours(hour, 1)].icon,
+//         ly_hour1Temp : Math.round(data.days[0].hours[setHours(hour, 1)].temp),
+//         ly_hour2Icon : data.days[0].hours[setHours(hour, 2)].icon,
+//         ly_hour2Temp : Math.round(data.days[0].hours[setHours(hour, 2)].temp),
+//         ly_hour3Icon : data.days[0].hours[setHours(hour, 3)].icon,
+//         ly_hour3Temp : Math.round(data.days[0].hours[setHours(hour, 3)].temp),
+//         ly_hour4Icon : data.days[0].hours[setHours(hour, 4)].icon,
+//         ly_hour4Temp : Math.round(data.days[0].hours[setHours(hour, 4)].temp),
+//         ly_hour5Icon : data.days[0].hours[setHours(hour, 5)].icon,
+//         ly_hour5Temp : Math.round(data.days[0].hours[setHours(hour, 5)].temp), 
+
+//         ly_day0Day : "Today",
+//         ly_day0Icon : data.days[1].icon,
+//         ly_day0TempLow : Math.round(data.days[1].tempmin),
+//         ly_day0TempHigh : Math.round(data.days[1].tempmax),
+//         ly_day1Day : data.days[1].datetime,
+//         ly_day1Icon : data.days[1].icon,
+//         ly_day1TempLow : Math.round(data.days[1].tempmin),
+//         ly_day1TempHigh : Math.round(data.days[1].tempmax),
+//         ly_day2Day : data.days[2].datetime,
+//         ly_day2Icon : data.days[2].icon,
+//         ly_day2TempLow : Math.round(data.days[2].tempmin),
+//         ly_day2TempHigh : Math.round(data.days[2].tempmax),
+//         ly_day3Day : data.days[3].datetime,
+//         ly_day3Icon : data.days[3].icon,
+//         ly_day3TempLow : Math.round(data.days[3].tempmin),
+//         ly_day3TempHigh : Math.round(data.days[3].tempmax),
+//         ly_day4Day : data.days[4].datetime,
+//         ly_day4Icon : data.days[4].icon,
+//         ly_day4TempLow : Math.round(data.days[4].tempmin),
+//         ly_day4TempHigh : Math.round(data.days[4].tempmax),
+//         ly_day5Day : data.days[5].datetime,
+//         ly_day5Icon : data.days[5].icon,
+//         ly_day5TempLow : Math.round(data.days[5].tempmin),
+//         ly_day5TempHigh : Math.round(data.days[5].tempmax),
+//       };
+//       console.log(displayData);
+//       displayWeather(displayData);
+//     })
+//   }
+// }
 
 function displayWeather(displayData) {
   clearDisplay();
@@ -161,21 +235,21 @@ function displayWeather(displayData) {
   document.querySelector('#ly-hour4').textContent = setHours(displayData.datetime.slice(0, 2), 4);
   document.querySelector('#ly-hour5').textContent = setHours(displayData.datetime.slice(0, 2), 5);
 
-  document.querySelector('#ly-hour0-icon').src = getIcon(displayData.hour0Icon);
-  document.querySelector('#ly-hour1-icon').src = getIcon(displayData.hour1Icon);
-  document.querySelector('#ly-hour2-icon').src = getIcon(displayData.hour2Icon);
-  document.querySelector('#ly-hour3-icon').src = getIcon(displayData.hour3Icon);
-  document.querySelector('#ly-hour4-icon').src = getIcon(displayData.hour4Icon);
-  document.querySelector('#ly-hour5-icon').src = getIcon(displayData.hour5Icon);
+  document.querySelector('#ly-hour0-icon').src = getIcon(displayData.ly_hour0Icon);
+  document.querySelector('#ly-hour1-icon').src = getIcon(displayData.ly_hour1Icon);
+  document.querySelector('#ly-hour2-icon').src = getIcon(displayData.ly_hour2Icon);
+  document.querySelector('#ly-hour3-icon').src = getIcon(displayData.ly_hour3Icon);
+  document.querySelector('#ly-hour4-icon').src = getIcon(displayData.ly_hour4Icon);
+  document.querySelector('#ly-hour5-icon').src = getIcon(displayData.ly_hour5Icon);
 
-  document.querySelector('#ly-hour0-temp').textContent = `na°`;
-  document.querySelector('#ly-hour1-temp').textContent = `na°`;
-  document.querySelector('#ly-hour2-temp').textContent = `na°`;
-  document.querySelector('#ly-hour3-temp').textContent = `na°`;
-  document.querySelector('#ly-hour4-temp').textContent = `na°`;
-  document.querySelector('#ly-hour5-temp').textContent = `na°`;
+  document.querySelector('#ly-hour0-temp').textContent = `${displayData.ly_hour0Temp}°`;
+  document.querySelector('#ly-hour1-temp').textContent = `${displayData.ly_hour1Temp}°`;
+  document.querySelector('#ly-hour2-temp').textContent = `${displayData.ly_hour2Temp}°`;
+  document.querySelector('#ly-hour3-temp').textContent = `${displayData.ly_hour3Temp}°`;
+  document.querySelector('#ly-hour4-temp').textContent = `${displayData.ly_hour4Temp}°`;
+  document.querySelector('#ly-hour5-temp').textContent = `${displayData.ly_hour5Temp}°`;
 
-  document.querySelector('#ly-day0').textContent = "Today";
+  document.querySelector('#ly-day0').textContent = displayData.ly_day0Day;
   document.querySelector('#ly-day1').textContent = setDay(displayData.day1Day);
   document.querySelector('#ly-day2').textContent = setDay(displayData.day2Day);
   document.querySelector('#ly-day3').textContent = setDay(displayData.day3Day);
@@ -189,19 +263,19 @@ function displayWeather(displayData) {
   document.querySelector('#ly-day4-icon').src = getIcon(displayData.day4Icon);
   document.querySelector('#ly-day5-icon').src = getIcon(displayData.day5Icon);
 
-  document.querySelector('#ly-day0-low-temp').textContent = `na°`;
-  document.querySelector('#ly-day1-low-temp').textContent = `na°`;
-  document.querySelector('#ly-day2-low-temp').textContent = `na°`;
-  document.querySelector('#ly-day3-low-temp').textContent = `na°`;
-  document.querySelector('#ly-day4-low-temp').textContent = `na°`;
-  document.querySelector('#ly-day5-low-temp').textContent = `na°`;
+  document.querySelector('#ly-day0-low-temp').textContent = `${displayData.ly_day0TempLow}°`;
+  document.querySelector('#ly-day1-low-temp').textContent = `${displayData.ly_day1TempLow}°`;
+  document.querySelector('#ly-day2-low-temp').textContent = `${displayData.ly_day2TempLow}°`;
+  document.querySelector('#ly-day3-low-temp').textContent = `${displayData.ly_day3TempLow}°`;
+  document.querySelector('#ly-day4-low-temp').textContent = `${displayData.ly_day4TempLow}°`;
+  document.querySelector('#ly-day5-low-temp').textContent = `${displayData.ly_day5TempLow}°`;
 
-  document.querySelector('#ly-day0-high-temp').textContent = `na°`;
-  document.querySelector('#ly-day1-high-temp').textContent = `na°`;
-  document.querySelector('#ly-day2-high-temp').textContent = `na°`;
-  document.querySelector('#ly-day3-high-temp').textContent = `na°`;
-  document.querySelector('#ly-day4-high-temp').textContent = `na°`;
-  document.querySelector('#ly-day5-high-temp').textContent = `na°`;
+  document.querySelector('#ly-day0-high-temp').textContent = `${displayData.ly_day0TempHigh}°`;
+  document.querySelector('#ly-day1-high-temp').textContent = `${displayData.ly_day1TempHigh}°`;
+  document.querySelector('#ly-day2-high-temp').textContent = `${displayData.ly_day2TempHigh}°`;
+  document.querySelector('#ly-day3-high-temp').textContent = `${displayData.ly_day3TempHigh}°`;
+  document.querySelector('#ly-day4-high-temp').textContent = `${displayData.ly_day4TempHigh}°`;
+  document.querySelector('#ly-day5-high-temp').textContent = `${displayData.ly_day5TempHigh}°`;
 }
 
 function cleanLocation(location) {
